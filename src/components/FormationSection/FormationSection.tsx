5b45d1b88e2646689ed84e981cb4e7c6e88f7bad
Dmytrii Shypilov <dmytriishypilov@gmail.com>
2024-09-20 17:17:37 +0300

    Update FormationSection.tsx

--- src/components/FormationSection/FormationSection.tsx
+++ src/components/FormationSection/FormationSection.tsx
@@ -8,7 +8,7 @@ import Transition from "../ui/Transition/Transition";
 const FormationSection: React.FC = () => {
   return (
     <>
-      <section className={s.section} id='formation'>
+      <section className={s.section} id="formation">
         <Container>
           <Heading
             animate="opacity"
@@ -17,15 +17,23 @@ const FormationSection: React.FC = () => {
             centered={true}
           />
           <div className={s.wrapper}>
-            <Transition as='div' animate="opacity" duration={0.7}> <Image
-              src="/images/corporate-office.webp"
-              className={s.image}
-              width={480}
-              height={376}
-              alt="corporate dubai"
-            /></Transition>
-           
-            <Transition as='div' animate="opacity" duration={0.8} className={s.textBox}>
+            <Transition as="div" animate="opacity" duration={0.7}>
+              {" "}
+              <Image
+                src="/images/corporate-office.webp"
+                className={s.image}
+                width={480}
+                height={376}
+                alt="corporate dubai"
+              />
+            </Transition>
+
+            <Transition
+              as="div"
+              animate="opacity"
+              duration={0.8}
+              className={s.textBox}
+            >
               <p className={s.text}>
                 At MCS, we understand that every business is unique, which is
                 why our corporate solutions are personalized to suit your
@@ -35,12 +43,28 @@ const FormationSection: React.FC = () => {
                 with the tailored support necessary to navigate complex markets
                 and achieve sustainable success.
               </p>
-             <ul>
-              <li className={s.list_item}><span className={s.icon}></span><span className={s.service}>Company Representation</span></li>
-              <li className={s.list_item}><span className={s.icon}></span><span className={s.service}>Company Formation Worldwide</span></li>
-              <li className={s.list_item}><span className={s.icon}></span><span className={s.service}>Opening Potential Clients and Markets</span></li>
-              <li className={s.list_item}><span className={s.icon}></span><span className={s.service}>Introduction to Private Equity and Venture Capital</span></li>
-             </ul>
+              <ul>
+                <li className={s.list_item}>
+                  <span className={s.icon}></span>
+                  <span className={s.service}>Company Representation</span>
+                </li>
+                <li className={s.list_item}>
+                  <span className={s.icon}></span>
+                  <span className={s.service}>Company Formation Worldwide</span>
+                </li>
+                <li className={s.list_item}>
+                  <span className={s.icon}></span>
+                  <span className={s.service}>
+                    Opening Potential Clients and Markets
+                  </span>
+                </li>
+                <li className={s.list_item}>
+                  <span className={s.icon}></span>
+                  <span className={s.service}>
+                    Introduction to Private Equity and Venture Capital
+                  </span>
+                </li>
+              </ul>
             </Transition>
           </div>
         </Container>
@@ -48,21 +72,27 @@ const FormationSection: React.FC = () => {
       <section className={s.services_section}>
         <Container>
           <ul className={s.services_list}>
-            <Transition as='li' animate='bubble' className={s.services_list_item}>
+            <Transition
+              as="li"
+              animate="bubble"
+              className={s.services_list_item}
+            >
               <div className={s.name_wrapper}>
                 <h3 className={s.service_name}>Company Representation</h3>
               </div>
 
               <p className={s.service_text}>
-                MCS acts as an exclusive representative for international
-                businesses seeking to expand into the UAE, GCC, and Africa,
-                ensuring seamless market entry and strong government
-                relationships. With deep local expertise, MCS facilitates
-                partnerships between Western and Far Eastern companies and
-                regional stakeholders.
+                With deep local expertise, MCS acts as an exclusive
+                representative for international businesses seeking to expand
+                into the UAE, GCC, and Africa, ensuring seamless market entry
+                and strong government relationships.
               </p>
             </Transition>
-            <Transition as='li' animate='bubble' className={s.services_list_item}>
+            <Transition
+              as="li"
+              animate="bubble"
+              className={s.services_list_item}
+            >
               <div className={s.name_wrapper}>
                 <h3 className={s.service_name}>Company Formation Worldwide</h3>
               </div>
@@ -74,7 +104,11 @@ const FormationSection: React.FC = () => {
                 markets.
               </p>
             </Transition>
-            <Transition as='li' animate='bubble' className={s.services_list_item}>
+            <Transition
+              as="li"
+              animate="bubble"
+              className={s.services_list_item}
+            >
               <div className={s.name_wrapper}>
                 <h3 className={s.service_name}>
                   Opening Potential Clients and Markets
@@ -88,7 +122,11 @@ const FormationSection: React.FC = () => {
                 industries.
               </p>
             </Transition>
-            <Transition as='li' animate='bubble' className={s.services_list_item}>
+            <Transition
+              as="li"
+              animate="bubble"
+              className={s.services_list_item}
+            >
               <div className={s.name_wrapper}>
                 <h3 className={s.service_name}>
                   Introduction to Private Equity and Venture Capital
